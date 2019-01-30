2f6931eda5e56a693d35a18b77d7c237cccd3db5
davejmacm <davejmacm@gmail.com>
2019-01-30 21:28:07 +0000

    mvp achieved

--- models/traveller.js
+++ models/traveller.js
@@ -3,19 +3,27 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  return this.journeys.map((journey) =>{
+    return journey.startLocation;
+  });
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  return this.journeys.map((journey) =>{
+    return journey.endLocation;
+  });
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter((journey) =>{
+    return journey.transport === transport;
+  });
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter((journey) => {
+    return journey.distance >= minDistance ;
+  });
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
